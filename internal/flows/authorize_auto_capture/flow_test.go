c26422193c4786849e0980358e52f5feb422e434
ctrlprogrammer <sebastianto1999@gmail.com>
2025-11-03 05:28:01 -0500

    Test authorize aut capture

--- internal/flows/authorize_auto_capture/flow_test.go
+++ internal/flows/authorize_auto_capture/flow_test.go
@@ -2,13 +2,15 @@ package flows
 
 import (
 	adapters_akua "akua-project/internal/adapters/akua"
+	adapters_akua_authorization "akua-project/internal/adapters/akua/authorization"
+	"akua-project/internal/instruments"
 	"context"
-	"fmt"
 	"log"
 	"path/filepath"
 	"testing"
 
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 )
 
 func InitializeEnvVariables() error {
@@ -23,19 +25,19 @@ func InitializeEnvVariables() error {
 	return nil
 }
 
-func InitializePaymentFlow(ctx context.Context) error {
+func InitializePaymentFlow(ctx context.Context) (*adapters_akua.Client, *adapters_akua_authorization.AuthorizationProvider, error) {
 	InitializeEnvVariables()
 
 	akuaClient, err := adapters_akua.NewClient()
 
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 
 	err = akuaClient.LoadJwtToken()
 
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 
 	log.Println("Starting payment flow initialization...")
@@ -55,15 +57,15 @@ func InitializePaymentFlow(ctx context.Context) error {
 		log.Println("JWT token loaded successfully.")
 	}
 
-	return nil
+	return akuaClient, adapters_akua_authorization.NewAuthorizationProvider(), nil
 }
 
-func Test_Authorize_Auto_Capture_Success(t *testing.T) {
+func Test_Authorize_AutoCapture_Success(t *testing.T) {
 	log.Println("=================================================")
 	log.Println("Testing Authorize with automatic capture...")
 	log.Println("=================================================")
 
-	err := InitializePaymentFlow(context.Background())
+	akuaClient, authorizationProvider, err := InitializePaymentFlow(context.Background())
 
 	if err != nil {
 		t.Fatal(err)
@@ -73,5 +75,57 @@ func Test_Authorize_Auto_Capture_Success(t *testing.T) {
 	log.Println("Finish flow initialization...")
 	log.Println("=================================================")
 
-	fmt.Println("Payment Flow")
+	log.Println("=================================================")
+	log.Println("Start authorization...")
+	log.Println("=================================================")
+
+	request := adapters_akua_authorization.AuthorizeRequest{
+		Amount: instruments.AmountObject{
+			Currency: "USD",
+			Value:    100,
+		},
+		Intent:     adapters_akua_authorization.INTENT_AUTHORIZE,
+		MerchantId: akuaClient.GetMerchantId(),
+		Capture: instruments.CaptureObject{
+			Mode: instruments.CAPTURE_MODE_AUTOMATIC,
+		},
+		Instrument: instruments.InstrumentObject{
+			Type: "CARD",
+			Card: instruments.Instrument{
+				Number:          instruments.CARD_APPROVED_CREDIT,
+				CVV:             "123",
+				ExpirationMonth: "12",
+				ExpirationYear:  "26",
+				HolderName:      "John Doe",
+			},
+		},
+	}
+
+	log.Println("=================================================")
+	log.Println("Authorization Request:")
+	log.Println("Amount: ", request.Amount)
+	log.Println("Intent: ", request.Intent)
+	log.Println("MerchantId: ", request.MerchantId)
+	log.Println("Capture Mode: ", request.Capture.Mode)
+	log.Println("Instrument: ", request.Instrument)
+	log.Println("=================================================")
+
+	authorization, err := authorizationProvider.Authorize(context.Background(), akuaClient, request)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Println("=================================================")
+	log.Println("Authorization Response:")
+	log.Println("Authorization ID: ", authorization.PaymentID)
+	log.Println("Authorization Status: ", authorization.Transaction.Status)
+	log.Println("Authorization Amount: ", authorization.Transaction.Amount)
+	log.Println("Authorization Transaction ID: ", authorization.Transaction.ID)
+	log.Println("Authorization Transaction Status: ", authorization.Transaction.Status)
+	log.Println("=================================================")
+
+	assert.NotNil(t, authorization)
+	assert.NotNil(t, authorization.PaymentID)
+	assert.Equal(t, "APPROVED", authorization.Transaction.Status, "expected transaction status to be APPROVED")
 }
